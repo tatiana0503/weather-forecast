ab5e2211609d3437a9ffa057b03b69940160e394
tatiana0503 <tzhamoitina@gmail.com>
2024-04-03 10:12:32 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+<h1>Weather Forecast</h1>
+<p>This application allows you to get the current weather forecast for any city in the world</p>
+
+<img src="weather.png" width="500px">
+
+<h3>Technology used:</h3>
+<ul>
+   <li>HTML</li>
+   <li>CSS</li>
+   <li>JavaScript</li>
+</ul>
---
+++ script.js
@@ -0,0 +1,55 @@
+const api = {
+    endpoint: "https://api.openweathermap.org/data/2.5/",
+    key: "902a123460c1cbd1f5cac24f0de07e64"
+}
+ const input = document.querySelector('#input');
+ input.addEventListener('keypress', enter);
+ 
+
+ function enter(e){
+    if(e.keyCode===13){
+        getInfo(input.value);
+    }
+}
+
+async function getInfo(data){
+    const res = await fetch(`${api.endpoint}weather?q=${data}&units=metric&appID=${api.key}`);
+    const result = await res.json();
+    displayResult(result);
+    
+  }
+
+ function displayResult(result){
+    let city = document.querySelector('#city');
+    city.textContent = `${result.name}, ${result.sys.country}`;
+
+    getOurDate();
+
+    let temperature = document.querySelector('#temperature');
+    temperature.innerHTML = `${Math.round(result.main.temp)}<span>°</span>`;
+
+    let feelsLike = document.querySelector('#feelsLike');
+    feelsLike.innerHTML = 'Feels like: '+ `${Math.round(result.main.feels_like)}<span>°</span>`;
+
+    let conditions = document.querySelector('#conditions');
+    conditions.textContent= `${result.ewather[0].main}`;
+
+    let variation = document.querySelector('#variation');
+    variation.innerHTML = 'Min: '+`${Math.round(result.main.temp_min)}<span>°</span>` + 'Max: ' + `${Math.round(result.main.temp_max)}<span>°</span>`;
+        
+ }
+
+ function getOurDate(){
+    const myDate = new Date();
+    const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+    let day = days[myDate.getDay()];
+    let todayDate = myDate.getDate();
+    let month = months[myDate.getMonth()];
+    let year = myDate.getFullYear();
+
+    let showDate = document.querySelector('#date');
+    showDate.textContent = `${day}`+" "+`${todayDate}`+" "+`${month}`+" "+`${year}`;
+
+ }
